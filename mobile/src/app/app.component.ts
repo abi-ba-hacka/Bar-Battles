e5b2d9d4de875ce0f4954a17924c3eda694478b7
Sebastian Giro <giro.sebastian@gmail.com>
2017-04-08 10:47:36 -0300

    Updated pages in app.component

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -6,6 +6,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { NativeStorage } from 'ionic-native';
 
 import { AuthPageComponent } from '../pages/+auth/auth.component';
+import { LoginPageComponent } from '../pages/+auth/+login/login.component';
+
+// pages
+import { ClientPageComponent } from '../pages/+client/client.component';
+import { BarStatsPageComponent } from '../pages/+admin/bar-stats/bar-stats.component';
+import { BattleTabPageComponent } from '../pages/+admin/battle-tab/battle-tab.component';
+import { DiscountsPageComponent } from '../pages/+admin/discounts/discounts.component';
+import { GoalsPageComponent } from '../pages/+admin/goals/goals.component';
+import { QRGeneratorPageComponent } from '../pages/+admin/qr-generator/qr-generator.component';
 
 import { Store } from '@ngrx/store';
 
@@ -23,6 +32,15 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      NativeStorage.getItem('user')
+      .then((data) => {
+        this.nav.push(ClientPageComponent);
+        splashScreen.hide();
+      }, (error) => {
+        this.nav.push(LoginPageComponent);
+        splashScreen.hide();
+      });
+
       statusBar.styleDefault();
       splashScreen.hide();
     });
