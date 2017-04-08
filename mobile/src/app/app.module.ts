ccfe8e82ebd28057f81d3a7b4467eaa97ffec92e
Sebastian Giro <giro.sebastian@gmail.com>
2017-04-08 10:43:20 -0300

    Added admin components to admin module

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -3,22 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
 import { AuthPageComponent } from '../pages/+auth/auth.component';
 import { LoginPageComponent } from '../pages/+auth/+login/login.component';
 
-import { AdminPageComponent } from '../pages/+admin/admin.component';
 import { ClientPageComponent } from '../pages/+client/client.component';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { AdminPageComponent } from '../pages/+admin/admin.component';
+import { BarStatsPageComponent } from '../pages/+admin/bar-stats/bar-stats.component';
+import { BattleTabPageComponent } from '../pages/+admin/battle-tab/battle-tab.component';
+import { DiscountsPageComponent } from '../pages/+admin/discounts/discounts.component';
+import { GoalsPageComponent } from '../pages/+admin/goals/goals.component';
+import { QRGeneratorPageComponent } from '../pages/+admin/qr-generator/qr-generator.component';
 
 @NgModule({
   declarations: [
     MyApp,
     AuthPageComponent,
     LoginPageComponent,
-    AdminPageComponent,
+
     ClientPageComponent,
+
+    AdminPageComponent,
+    BarStatsPageComponent,
+    BattleTabPageComponent,
+    DiscountsPageComponent,
+    GoalsPageComponent,
+    QRGeneratorPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,8 +42,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     LoginPageComponent,
     AuthPageComponent,
-    AdminPageComponent,
+
     ClientPageComponent,
+
+    AdminPageComponent,
+    BarStatsPageComponent,
+    BattleTabPageComponent,
+    DiscountsPageComponent,
+    GoalsPageComponent,
+    QRGeneratorPageComponent,
   ],
   providers: [
     StatusBar,
